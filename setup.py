81753a5de0684be38245f84cff0b29f19482f0f7
rachmadaniHaryono <foreturiga@gmail.com>
2017-08-13 18:39:02 +0800

    new: test: pylint test

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 """setup file."""
-from codecs import open
 from os import path
 
 from setuptools import find_packages, setup
@@ -9,7 +8,7 @@ __version__ = '0.0.1'
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 install_requires = [
