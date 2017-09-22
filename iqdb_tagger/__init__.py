77decc91dae8d059a9fb63c0889194578dc9af37
q <foreturiga@gmail.com>
2017-09-22 13:24:49 +0800

    chg: doc: update version

--- iqdb_tagger/__init__.py
+++ iqdb_tagger/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Init file."""
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 db_version = 1
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 from setuptools import find_packages, setup
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 here = path.abspath(path.dirname(__file__))
 
