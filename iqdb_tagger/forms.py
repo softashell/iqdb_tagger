84c3c5b1b1eeca2793d9a085a578ff08bbd3f334
rachmadaniHaryono <foreturiga@gmail.com>
2018-03-28 20:59:25 +0800

    fix: dev: use FlaskForm to remove warning

--- iqdb_tagger/forms.py
+++ iqdb_tagger/forms.py
@@ -1,11 +1,11 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SelectField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 from . import models
 
 
-class ImageUploadForm(Form):
+class ImageUploadForm(FlaskForm):
     file = FileField('Image', validators=[
         FileRequired(),
         FileAllowed(['jpg', 'png'], 'Images only!')
